c6e1729da7398e5d2aa3f4e670301f9acee49985
Anton Fedorenko <anthony.fedorenko@gmail.com>
2026-08-08 21:24:41 +0200

    revert: disable image optimizer (EXIF orientation stripped)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      jpg: { quality: 85 },
-      jpeg: { quality: 85 },
-      png: { quality: 85 },
-      webp: { lossless: false, quality: 85 },
-    }),
-  ],
+  plugins: [react()],
 });
