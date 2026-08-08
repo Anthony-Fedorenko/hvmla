846cf9423df7a14fbd95b158c14064ff3006359c
Anton Fedorenko <anthony.fedorenko@gmail.com>
2026-08-08 21:28:43 +0200

    feat: image optimizer with metadata preservation

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: { quality: 85 },
+      jpeg: { quality: 85 },
+      png: { quality: 85 },
+      webp: { lossless: false, quality: 85 },
+      withMetadata: true,
+    }),
+  ],
 });
